8d9df20d979c6c35799c8ba46ce72e95ba4ed69f
Kenny Peng <pengk@alum.mit.edu>
2012-01-16 23:22:48 -0500

    Untabify visionyc.js, this is what happens when you use a new editor

--- static/visionyc.js
+++ static/visionyc.js
@@ -13,28 +13,28 @@ var projection = d3.geo.equirectangular();
 var path = d3.geo.path().projection(projection);
 
 function format(number) {
-	number += '';
-	x = number.split('.');
-	x1 = x[0];
-	x2 = x.length > 1 ? '.' + x[1].substr(0, 2) : '';
-	var regexp = /(\d+)(\d{3})/;
-	while (regexp.test(x1))
-		x1 = x1.replace(regexp, '$1' + ',' + '$2');
-	return x1 + x2;
+    number += '';
+    x = number.split('.');
+    x1 = x[0];
+    x2 = x.length > 1 ? '.' + x[1].substr(0, 2) : '';
+    var regexp = /(\d+)(\d{3})/;
+    while (regexp.test(x1))
+        x1 = x1.replace(regexp, '$1' + ',' + '$2');
+    return x1 + x2;
 }
 
 d3.json('data/zipcodes.json', function(json) {
     //
     // Set up zip code boundary features
     //
-
+    
     var features = json.features;
-
+    
     var svg = d3.select('#chart').append('svg')
         .attr('width', width)
         .attr('height', height)
         .call(d3.behavior.zoom().on('zoom', redraw));
-
+    
     var zipcodes = svg.append('g')
         .attr('id', 'nyc-zipcodes')
         .attr('class', 'Blues');
@@ -99,87 +99,87 @@ d3.json('data/zipcodes.json', function(json) {
     // Set up choropleth electricity_usage
     //
 
-	d3.json('data/population.json', function(population_data) {
-		population = population_data;
-
-		d3.json('data/electricity.json', function(json) {
-			electricity_usage = json;
-
-			var values = [];
-			for (var k in electricity_usage) {
-				if (electricity_usage[k][type])
-					values.push(electricity_usage[k][type]);
-			}
-			values.sort(function(a, b) { return a - b; });
-
-			var ranges = [];
-			for (var i = 0; i < values.length; ++i)
-				ranges[i] = Math.floor(i / (Math.ceil(values.length / 9)));
-
-			quantile = d3.scale.ordinal()
-				.domain(values)
-				.range(ranges);
-
-			zipcodes.selectAll('path')
-				.attr('original-title', function(d) {
-					var output = electricity_usage[d.id] ? (electricity_usage[d.id][type] ? electricity_usage[d.id][type] : 0) : 0;
-					return d.id + ': ' + format(output);
-				})
-				.html(function(d) {
-					$(this).tipsy({ gravity: 'e' });
-				});
-
-			zipcodes.selectAll('path')
-				.attr('class', quantize);
-		});
-	});
+    d3.json('data/population.json', function(population_data) {
+        population = population_data;
+        
+        d3.json('data/electricity.json', function(json) {
+            electricity_usage = json;
+            
+            var values = [];
+            for (var k in electricity_usage) {
+                if (electricity_usage[k][type])
+                    values.push(electricity_usage[k][type]);
+            }
+            values.sort(function(a, b) { return a - b; });
+            
+            var ranges = [];
+            for (var i = 0; i < values.length; ++i)
+                ranges[i] = Math.floor(i / (Math.ceil(values.length / 9)));
+            
+            quantile = d3.scale.ordinal()
+                .domain(values)
+                .range(ranges);
+            
+            zipcodes.selectAll('path')
+                .attr('original-title', function(d) {
+                    var output = electricity_usage[d.id] ? (electricity_usage[d.id][type] ? electricity_usage[d.id][type] : 0) : 0;
+                    return d.id + ': ' + format(output);
+                })
+                .html(function(d) {
+                    $(this).tipsy({ gravity: 'e' });
+                });
+            
+            zipcodes.selectAll('path')
+                .attr('class', quantize);
+        });
+    });
 
     function quantize(d) {
-		var type_alias = type == 'residential-per-capita' ? 'residential' : type;
+        var type_alias = type == 'residential-per-capita' ? 'residential' : type;
         if (electricity_usage[d.id] && electricity_usage[d.id][type_alias])
             return 'q' + ~~quantile(electricity_usage[d.id][type_alias]) + '-9';
         else
             return 'q0-9';
     }
-
-	//
-	// Handle choropleth transitions for drop-down list
-	//
-
-	$('#residence-type').change(function(ui) {
-		type = ui.target.value;
-
+    
+    //
+    // Handle choropleth transitions for drop-down list
+    //
+    
+    $('#residence-type').change(function(ui) {
+        type = ui.target.value;
+        
         var values = [];
         for (var k in electricity_usage) {
-			var type_alias = type == 'residential-per-capita' ? 'residential' : type;
+            var type_alias = type == 'residential-per-capita' ? 'residential' : type;
             if (electricity_usage[k][type_alias]) {
-				if (type == 'residential-per-capita')
-	                values.push(electricity_usage[k][type_alias] / population[k]);
-				else
-	                values.push(electricity_usage[k][type_alias]);
+                if (type == 'residential-per-capita')
+                    values.push(electricity_usage[k][type_alias] / population[k]);
+                else
+                    values.push(electricity_usage[k][type_alias]);
             }
         }
         values.sort(function(a, b) { return a - b; });
-
-		var ranges = [];
-		for (var i = 0; i < values.length; ++i)
-			ranges[i] = Math.floor(i / (Math.ceil(values.length / 9)));
-
+        
+        var ranges = [];
+        for (var i = 0; i < values.length; ++i)
+            ranges[i] = Math.floor(i / (Math.ceil(values.length / 9)));
+        
         quantile = d3.scale.ordinal()
             .domain(values)
             .range(ranges);
-
+        
         zipcodes.selectAll('path')
             .attr('original-title', function(d) {
-				var type_alias = type == 'residential-per-capita' ? 'residential' : type;
-				var usage = electricity_usage[d.id];
+                var type_alias = type == 'residential-per-capita' ? 'residential' : type;
+                var usage = electricity_usage[d.id];
                 var output = usage ? (usage[type_alias] ?
-									  (type == 'residential-per-capita' ?
-									   usage[type_alias] / population[d.id] : usage[type_alias]) : 0) : 0;
+                                      (type == 'residential-per-capita' ?
+                                       usage[type_alias] / population[d.id] : usage[type_alias]) : 0) : 0;
                 return d.id + ': ' + format(output);
             });
-
+        
         zipcodes.selectAll('path')
             .attr('class', quantize);
-	});
+    });
 });
